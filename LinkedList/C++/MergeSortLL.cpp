95b8b6cc5f3e99475c8d274e7d2892075a1aeb45
amogh <amoghag2003@gmail.com>
2022-10-18 01:54:20 +0530

    Merge sort on linked list logic

---
+++ LinkedList/C++/MergeSortLL.cpp
@@ -0,0 +1,90 @@
+
+class Solution
+{
+public:
+    // void printLL(Node* head){
+    //     while(head!=NULL){
+    //         cout<<head->data<<" ";
+    //         head = head->next;
+    //     }
+    //     cout<<endl;
+    // }
+    // function to find mid
+    Node *find_mid(Node *head)
+    {
+        if (head->next == NULL)
+        {
+            return head;
+        }
+        Node *slow = head;
+        Node *fast = head->next;
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    // function to merge two sorted linked list;
+    Node *merge(Node *left, Node *right)
+    {
+        if (left == NULL)
+        {
+            return right;
+        }
+        if (right == NULL)
+        {
+            return left;
+        }
+        Node *ans = new Node(-1);
+        Node *temp = ans;
+        while (left != NULL && right != NULL)
+        {
+            if (left->data < right->data)
+            {
+                temp->next = left;
+                temp = left;
+                left = left->next;
+            }
+            else
+            {
+                temp->next = right;
+                temp = right;
+                right = right->next;
+            }
+        }
+        while (left != NULL)
+        {
+            temp->next = left;
+            temp = left;
+            left = left->next;
+        }
+        while (right != NULL)
+        {
+            temp->next = right;
+            temp = right;
+            right = right->next;
+        }
+        ans = ans->next;
+        return ans;
+    }
+
+    // Function to sort the given linked list using Merge Sort.
+    Node *mergeSort(Node *head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+        Node *mid = find_mid(head);
+        Node *left = head;
+        Node *right = mid->next;
+        mid->next = NULL;
+
+        left = mergeSort(left);
+        right = mergeSort(right);
+        Node *result = merge(left, right);
+        return result;
+    }
+};
