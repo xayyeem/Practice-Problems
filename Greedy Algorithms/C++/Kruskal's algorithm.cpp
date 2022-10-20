c75bed2ef9d271f4343c0baf518d06320ffb65fc
Mitanshilodha <109990196+Mitanshilodha@users.noreply.github.com>
2022-10-20 16:07:35 +0530

    Update Kruskal's algorithm.cpp
    Minimum Spanning tree using greedy

--- Greedy Algorithms/C++/Kruskal's algorithm.cpp
+++ Greedy Algorithms/C++/Kruskal's algorithm.cpp
@@ -104,4 +104,15 @@ int main()
     // Function call
     g.kruskals_mst();
     return 0;
+    
+    
 }
+
+/*
+Output-
+Following are the edges in the constructed MST
+2 -- 3 == 4
+0 -- 3 == 5
+0 -- 1 == 10
+Minimum Cost Spanning Tree: 19
+*/
